cf1d86a11931b8bec9abd334191c480090e886d9
Garrett Smith <ggasmithh@users.noreply.github.com>
2018-06-10 22:41:33 +0200

    Added "missing fields" error to search function.

--- insert_vip_results.php
+++ insert_vip_results.php
@@ -5,25 +5,35 @@
 	<body>
 
 		<?php	
+			
+			if(!($_POST["SSN"] == '' || $_POST["Name"] == '' || $_POST["Surname"] == '' || $_POST["Employment"] == '')) {
 
-			$db = new mysqli('localhost', 'user', 'passwd', 'tv_apps', NULL, '/run/mysqld/mysqld.sock');
+				$db = new mysqli('localhost', 'user', 'passwd', 'tv_apps', NULL, '/run/mysqld/mysqld.sock');
 
-			if($db->connect_errno > 0){
-				die('Unable to connect to database [' . $db->connect_error . ']');
-			}
+				if($db->connect_errno > 0){
+					die('Unable to connect to database [' . $db->connect_error . ']');
+				}
 
-        		$sql = "INSERT INTO VIP (Ssn, Name, Surname, Employment) VALUES ('".$_POST["SSN"]."','".$_POST["Name"]."','".$_POST["Surname"]."','".$_POST["Employment"]."')";
+	        		$sql = "INSERT INTO VIP (Ssn, Name, Surname, Employment) VALUES ('".$_POST["SSN"]."','".$_POST["Name"]."','".$_POST["Surname"]."','".$_POST["Employment"]."')";
 		
-			if(!$result = $db->query($sql)){
-				die('There was an error running the query [' . $db->error . ']');
-			}
+				if(!$result = $db->query($sql)){
+					die('There was an error running the query [' . $db->error . ']');
+				}
 		
+				else {
+					echo "VIP added to Database";
+				}
+			
+			} 
+			
 			else {
-				echo "VIP added to Database";
-				echo '	<form>
-						<input type="button" value="New Insertion" onclick="window.location.href=\'insert_vip_home.php\'" />
-					</form>';
+				echo "ERROR: All fields must be filled for insertion";
 			}
+			
+			echo '	<form>
+					<input type="button" value="New Insertion" onclick="window.location.href=\'insert_vip_home.php\'" />
+				</form>';
+				
 		?>
 
 	</body>
